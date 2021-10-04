69267906d5e3931de6353482ee997fc2a680dc1c
Cesar Guzman <47696662+CesarGuzmanLopez@users.noreply.github.com>
2021-10-04 14:21:28 -0500

    as

--- src/main/java/LauCesar/AnalizadorAmbiente/views/Graficar.java
+++ src/main/java/LauCesar/AnalizadorAmbiente/views/Graficar.java
@@ -1,37 +1,118 @@
 package LauCesar.AnalizadorAmbiente.views;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.general.AbstractDataset;
-import org.jfree.data.xy.DefaultXYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
-public class Graficar {
-    public static void main(String[] args) {
-        UpdatingRandomCategoryDataset set = new UpdatingRandomCategoryDataset(5, 5, 100);
-        JFreeChart chart = ChartFactory.createBarChart(
-            "Dynamic Data","x","y",
-            set,PlotOrientation.VERTICAL,true, true, false);
-        JFrame frame = new JFrame("Test");
-        frame.setContentPane(new ChartPanel(chart));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-        for (int i = 1; i < 200; i++){
-            set.update();
-            try{
-                Thread.sleep(5);
-            }
-            catch (InterruptedException e){
-            }       
-        }
-    }
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Graficar extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Graficar frame = new Graficar();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+
+	/**
+	 * Create the frame.
+	 */
+	public Graficar() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 597, 470);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	
+	
+		
+        XYSeriesCollection data = new XYSeriesCollection();
+    	SensorEstatico tempe = new SensorEstatico("temperatura");
+        data.addSeries(tempe);	
+		JFreeChart chart = ChartFactory.createXYStepChart(  "temperatura","x","y",data,PlotOrientation.VERTICAL,true, true, false);
+	
+
+		ChartPanel panel = new ChartPanel(chart);
+		
+		//JFreeChart a2 = ChartFactory.createLineChart(  "","x","y",temperatura,PlotOrientation.VERTICAL,true, true, false);
+	
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(53, 100, 503, 307);
+		
+		contentPane.add(panel);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tempe.reset();
+			}
+		});
+		btnNewButton.setBounds(29, 26, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		setVisible(true);
+	}
 }
+
+class SensorEstatico extends XYSeries {
+
+	public SensorEstatico(Comparable key) {
+		super(key);
+		   add(0, 1);
+	       add(1, 5);
+	       add(2, 5);
+	       add(3, 5);
+	       add(4, 41);
+	       add(5, 22);
+	       add(6, 151);
+	       add(7, 40);
+	       add(8, 151);
+	}
+	public void reset(){
+		System.out.println(getMinX());
+		this.remove(this.getMinX());
+        Random random = new Random();
+		this.add(this.getMaxX()+1,random.nextInt(100));
+	}
+}
+
+
 class UpdatingRandomCategoryDataset extends AbstractDataset implements CategoryDataset{
     private int columnCount;
     private int rowCount;
