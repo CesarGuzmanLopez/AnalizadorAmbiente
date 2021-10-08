aeae8de1be4d38c2cf2f2f0bee556925fd91078a
Cesar Guzman <47696662+CesarGuzmanLopez@users.noreply.github.com>
2021-10-08 08:42:54 -0500

    mensaje

--- src/main/java/LauCesar/AnalizadorAmbiente/services/ArduinoToJava.java
+++ src/main/java/LauCesar/AnalizadorAmbiente/services/ArduinoToJava.java
@@ -1,6 +1,5 @@
 package LauCesar.AnalizadorAmbiente.services;
 
-import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
 import javax.swing.JTextArea;
@@ -20,7 +19,7 @@ import LauCesar.AnalizadorAmbiente.Modelos.*;
 public class ArduinoToJava implements Runnable {
 	private List<DatoSensor> Data;
 	private Object o;
-	private volatile boolean pause;
+	private volatile boolean pause,leyendo;
 	private volatile boolean finalizado;
 	private int numDatos;
 	JTextArea text;
@@ -113,9 +112,10 @@ public class ArduinoToJava implements Runnable {
 	@Override
 	public void run() {
 		int i = 0;
+		leyendo =false;
 		while (!Thread.currentThread().isInterrupted() && i++ < 10000 && !finalizado) {
 			try {
-				if (!pause)
+				if (!pause && !leyendo)
 					AgregaUnDatoSensor();
 				else
 					synchronized (o) {
@@ -143,33 +143,47 @@ public class ArduinoToJava implements Runnable {
 
 	public void AgregaUnDatoSensor() throws InterruptedException, DispositivoNoConectado, ExcepcionLectura {
 		try {
-			Thread.sleep(100);
+			long i=10;
+			Thread.sleep(i);
 			ino.sendByte('H');
+			Thread.sleep(i);
 			ino.sendByte('p');
+			Thread.sleep(i);
 			ino.sendByte('t');
+			Thread.sleep(i);
 			ino.sendByte('h');
-			Thread.sleep(100);
+
 		} catch (ArduinoException e) {
 			throw new ExcepcionLectura(e.getMessage());
 		} catch (SerialPortException e) {
 			throw new DispositivoNoConectado(e.getMessage());
 		}
-		 
+
 	}
 
 	class Escucha_sensor implements SerialPortEventListener {
 		@Override
 		public void serialEvent(SerialPortEvent spe) {
 			try {
-				if (ino.isMessageAvailable()) {
-					String mensaje=ino.printMessage();
- 					numDatos++;
- 					String[] Separado = mensaje.split(":");
- 					if(Separado.length==2 && Separado[0].length()>1 && Separado[1].length()>1 ) {
- 						DatoSensor x = new DatoSensor(mensaje);
- 						Data.add(x);
- 						text.append(x.toString());
- 					}
+				leyendo=true;
+				while (ino.isMessageAvailable() ) {
+					if(pause)
+						continue;
+					
+					String mensaje = ino.printMessage();
+					String[] Separado = mensaje.split(":");
+					if (Separado.length == 2 && Separado[0].length() > 1 && Separado[1].length() > 1) {
+						DatoSensor x = new DatoSensor(mensaje);
+						Data.add(x);
+						if(text!=null)
+							text.append(x.toString());
+						numDatos++;
+					}
+					
+				}
+				leyendo =false;
+				synchronized (o) {
+					o.notifyAll();
 				}
 			} catch (SerialPortException | ArduinoException ex) {
 				System.out.println("Hay un error" + ex.getMessage());
