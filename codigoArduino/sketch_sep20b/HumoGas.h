386d7d610919aaaf7d4afeb85665451392c631df
CesarGuzman <47696662+CesarGuzmanLopez@users.noreply.github.com>
2021-10-09 19:58:01 -0500

    hola

---
+++ codigoArduino/sketch_sep20b/HumoGas.h
@@ -0,0 +1,52 @@
+#ifndef _HUMOGAS_H
+#define _HUMOGAS_H
+namespace HumoGasMQ2 {
+
+
+class SensorHG {
+    const int   resistenciaL = 5;
+    const int   ResistenciaLimpio = 9.83;
+    const float LPCurve[3] = {2.3, 0.21, -0.47} ;
+    const float HumoCurve[3] = {2.3, 0.53, -0.44};
+    float Ro =  10;
+    int EntradaAnalogica; //entrada Analogica MQ2
+  public:
+    SensorHG(int EntradaAnalogica) {
+      this->EntradaAnalogica = EntradaAnalogica;
+      Calibracion();
+    }
+    float Calibracion() {
+      int i;
+      float val = 0;
+      for (int i = 0; i < 50; i++) {                                                                         //tomar múltiples muestras
+        val += calc_res(analogRead(EntradaAnalogica));
+        delay(500);
+      }
+      val = val / 50;                                                                                       //calcular el valor medio
+      Ro = val / ResistenciaLimpio;
+    
+    }
+    float calc_res(int raw_adc)
+    {
+      return ( ((float)ResistenciaLimpio * (1023 - raw_adc) / raw_adc));
+    }
+    
+    float lecturaMQ() {
+      int i;
+      float rs = 0;
+      for (i = 0; i < 5; i++) {
+        rs += calc_res(analogRead(EntradaAnalogica));
+        delay(50);
+      }
+      rs = rs / 5;
+      return rs;
+    }
+    int porcentaje_gas() {
+      return (pow(10, (((log(lecturaMQ()) - LPCurve[1]) / LPCurve[2]) + LPCurve[0])));
+    }
+};
+}
+
+
+
+#endif
--- codigoArduino/sketch_sep20b/sketch_sep20b.ino
+++ codigoArduino/sketch_sep20b/sketch_sep20b.ino
@@ -1,69 +1,53 @@
-#include "DHT.h"
+#include "HumoGas.h"
+HumoGasMQ2::SensorHG *Sensorhg;
 
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-
-const int DHTPin = 5;     // what digital pin we're connected to
-
-DHT dht(DHTPin, DHTTYPE);
-char option = ' ';
 void setup() {
-   delay(100);
-   Serial.begin(9600);
-   
+  delay(100);
+  Serial.begin(9600);
+  Sensorhg =new HumoGasMQ2::SensorHG(0);
 
-   dht.begin();
 }
-
 void loop() {
-    
-   if(Serial.available() != 0){
-    option = Serial.read();
-    switch(option){
-      case('t'):
-          Serial.println("Temperatura:"+String(temperatura())+"\n");
-          break;
-      case('H'):
-          Serial.println("Humedad:"+String(humedad())+"\n");
-          break;
-       case('p'):
-          Serial.println("Polvo:"+String(polvo())+"\n");
-          break;
-       case('h'):
-          Serial.println("Humo:"+String(humo())+"\n");
-          break;
-       case('\n'):
-       case('\t'):
-       case('\0'):
-       case('.'):
-            break;        
-        default:
-            Serial.println(option+ " error");
-            break;
-      }
-   }
+  char option = '\0';
   
+  if (Serial.available() != 0) {
+    option = Serial.read();
+    switch (option) {
+      case ('t'):
+        Serial.println("Temperatura:" + String(temperatura()) + "\n");
+        break;
+      case ('H'):
+        Serial.println("Humedad:" + String(humedad()) + "\n");
+        break;
+      case ('p'):
+        Serial.println("Polvo:" + String(polvo()) + "\n");
+        break;
+      case ('h'):
+        Serial.println("Humo:" + String(humo()) + "\n");
+        break;
+      case ('\n'):
+      case ('\t'):
+      case ('\0'):
+      case ('.'):
+        break;
+      default:
+        Serial.println(option + " error");
+        break;
+    }
+  }
+
   delay(100);
 }
 
-float polvo(){
-  return 1;
-  }
-float humedad(){
-  return 2;
-  }
-float temperatura(){
-  return 3;
-  }
-float humo(){
+inline float polvo() {
   return 4;
-  }
+}
+float humedad() {
+  return 2;
+}
+float temperatura() {
+  return 3;
+}
+float humo() {
+  return Sensorhg->porcentaje_gas();
+}
